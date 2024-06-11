75236be067b96e3b83fb535faac4011485fa750d
David Isinta <david.isinta@gmail.com>
2024-06-11 12:57:44 +0300

    succefully connected to the supabase database managed to perform queries

---
+++ backend/supabase_conn.py
@@ -0,0 +1,21 @@
+
+import os
+from supabase import create_client, Client
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+
+
+
+def main():
+
+    print(f"url is {url}")
+    response = supabase.table('countries').select("*").execute()
+    print(response)
+
+
+
+if __name__ == "__main__":
+    main()
