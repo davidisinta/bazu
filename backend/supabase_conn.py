d02a652176fa6a308514b5c976ff0f0e6c874b19
David Isinta <david.isinta@gmail.com>
2024-06-11 13:19:04 +0300

    slight changes to supabase connection

--- backend/supabase_conn.py
+++ backend/supabase_conn.py
@@ -1,4 +1,3 @@
-
 import os
 from supabase import create_client, Client
 
@@ -7,11 +6,9 @@ key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
 
-
-
 def main():
 
-    print(f"url is {url}")
+    print(f"url is: {url}")
     response = supabase.table('countries').select("*").execute()
     print(response)
 
