8e18dc89c6e3c35cae746581374507e10403e013
David Isinta <david.isinta@gmail.com>
2024-06-11 14:57:37 +0300

    added insert fn that enables data entry without specifying id explicitly

--- backend/supabase_conn.py
+++ backend/supabase_conn.py
@@ -5,7 +5,6 @@ url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
-
 def main():
 
     print(f"url is: {url}")
@@ -13,6 +12,25 @@ def main():
     print(response)
 
 
+    print("----------------------------------")
+
+
+
+
+
+def insert():
+    ## test insert data to countries table
+
+    data, count = ((supabase.table('countries')
+                    .insert({"name": "Kenya"}))
+                   .execute())
+
+    print(data)
+    print(count)
+
+
+
 
 if __name__ == "__main__":
     main()
+    insert()
